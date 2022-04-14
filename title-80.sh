b0748908121416bd2bc138a237c91ca6a745074e
Liz <31071746+caltrop2075@users.noreply.github.com>
2022-04-14 09:58:01 -0400

    Add files via upload

--- title-80.sh
+++ title-80.sh
@@ -5,16 +5,28 @@
 # 80 column title
 # now recognizes \n
 
-echo -e "$1" > ~/temp/temp                         # input to multiline file
+if [ $# = 1 ]
+then
+   msg=$1
+else
+   msg=$3
+fi
+
 echo -n > ~/data/title.dat                         # reset title file
 
 wid=80
+echo -e $msg |
 while read lin                                     # format text to title file
 do
    len=${#lin}
    lft=$((($wid-4-$len)/2))
    rht=$(($wid-4-$len-$lft))
    printf "%"$lft"s%s%"$rht"s\n" "" "$lin" "" >> ~/data/title.dat
-done < ~/temp/temp
-# title.sh;exit
-title.sh -t line                                   # display title file
+done
+
+if [ $# = 1 ]
+then
+   title.sh
+else
+   title.sh $1 $2
+fi
