7aa4f98850856f741ac6aec2f9f2433153e9a654
Liz <31071746+caltrop2075@users.noreply.github.com>
2023-12-10 08:59:58 -0500

    Add files via upload

--- thx.sh
+++ thx.sh
@@ -2,12 +2,29 @@
 #------------------------------------------------------------------------------#
 #                            Programmed By Liz                                 #
 #------------------------------------------------------------------------------#
+# random clip player for startup
+# started out as THX only but can add more
+# sets volume level to 33%
+#
+#-------------------------------------------------------------------------------
 source ~/data/global.dat
 lvl=33
-
+#-------------------------------------------------------------------------------
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.hdmi-stereo $lvl% 2> /dev/null
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo $lvl% 2> /dev/null
 
-mplayer -quiet -noborder /home/caltrop/.videos/THX.mp4 &> /dev/null &
+r=$((RANDOM%3))               # to add more change mod number and add to case
+case $r in
+   0)
+      mplayer -quiet -noborder "/home/caltrop/.videos/THX I.mp4" &> /dev/null &
+      ;;
+   1)
+      mplayer -quiet -noborder "/home/caltrop/.videos/THX II.mp4" &> /dev/null &
+      ;;
+   2)
+      mplayer -quiet -noborder "/home/caltrop/.videos/THX III.mp4" &> /dev/null &
+      ;;
+esac
 
 notify-send -i "$i_cat" "THX" "volume set to $lvl%"
+#-------------------------------------------------------------------------------
